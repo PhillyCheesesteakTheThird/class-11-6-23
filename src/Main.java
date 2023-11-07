f2ea1f4dddd1a7187685a66312d47d2718547cd6
Jadan Kerr <badi1863@gmail.com>
2023-11-06 17:52:44 -0700

    stuff

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,35 @@ public class Main {
         zebras.add(new Zebra(33));
         zebras.add(new Zebra(12));
         zebras.add(new Zebra(789));
+        List<Double> nums = new ArrayList<>();
+        System.out.println(sum(nums));
+
+        for (int i=0; i< 900; i++){
+            double num = (i*1000)/Math.random();
+            nums.add(num);
+        }
+        System.out.println(sum(nums));
+
 
         for (int i = 0; i<900; i++){
             zebras.add(new Zebra(i));
         }
 
         System.out.println(recursiveMostStripes(zebras).numberOfStripes);
+        System.out.println(getZebraWithMostStripes(zebras).numberOfStripes);
 
+    }
+    public static Double sum(List<Double> nums){
+        if (nums.isEmpty()){
+            return 0.0;
+        }
+
+        double number = nums.get(0);
+        if(nums.size() == 1){
+            return number;
+        }
+
+        return number + sum(nums.subList(1, nums.size() -1 ));
     }
     public static Zebra getZebraWithMostStripes(List<Zebra> zebras){
         Zebra mostStripes = null;
